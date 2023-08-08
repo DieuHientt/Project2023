222cce6d04f62bb8a01253e0f2c3919c41669e74
LePhuocHuyHoang <lehoang023479@gmail.com>
2023-08-08 21:22:30 +0700

    8/8/2023

---
+++ Projet/src/main/java/com/alibou/keycloak/repository/categoryRepository.java
@@ -0,0 +1,10 @@
+package com.alibou.keycloak.repository;
+
+import com.hon.keycloak.model.category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.math.BigInteger;
+
+public interface categoryRepository extends JpaRepository<category, BigInteger> {
+}
+//
---
+++ Projet/src/main/java/com/alibou/keycloak/repository/outcomeRepository.java
@@ -0,0 +1,10 @@
+package com.alibou.keycloak.repository;
+
+import com.hon.keycloak.model.outcome;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.math.BigInteger;
+
+public interface outcomeRepository extends JpaRepository<outcome, BigInteger> {
+}
+//
